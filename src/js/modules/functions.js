edc34d3080c9834af0c64cb7396dc03ac8f8ced8
explt13 <az13rede4d@gmail.com>
2025-02-22 12:33:55 +0500

    Fix auth validation

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -249,10 +249,10 @@ export function setSpoilers () {
 				spoilerTitles.forEach(spoilerTitle => {
 					if (hideSpoilerBody && !spoilerTitle.classList.contains('_active')) {
 						spoilerTitle.removeAttribute('tabindex');
-						spoilerTitle.nextElementSibling.style.display = 'none';
+						spoilerTitle.nextElementSibling.hidden = true;
 					} else {
 						spoilerTitle.setAttribute('tabindex', '-1');
-						spoilerTitle.nextElementSibling.style.removeProperty('display');
+						spoilerTitle.nextElementSibling.hidden = false;
 					}
 				});
 			}
@@ -301,7 +301,7 @@ export const _slideUp = (target, duration = 500) => {
 		target.style.marginTop = 0;
 		target.style.marginBottom = 0;
 		window.setTimeout(() => {
-			target.style.display = 'none';
+			target.hidden = true;
 			target.style.removeProperty('height');
 			target.style.removeProperty('padding-top');
 			target.style.removeProperty('padding-bottom');
@@ -318,8 +318,8 @@ export const _slideUp = (target, duration = 500) => {
 export const _slideDown = (target, duration = 500) => {
 	if (!target.classList.contains('_slide')) {
 		target.classList.add('_slide');
-		if (target.style.display == 'none') {
-			target.style.removeProperty('display');
+		if (target.hidden === true) {
+			target.hidden = false;
 		}
 		let height = target.offsetHeight;
 		target.style.overflow = 'hidden';
@@ -347,7 +347,7 @@ export const _slideDown = (target, duration = 500) => {
 }
 
 export const _slideToggle = (target, duration = 500) => {
-	if (target.style.display == 'none') {
+	if (target.hidden === true) {
 		return _slideDown(target, duration);
 	} else {
 		return _slideUp(target, duration);
